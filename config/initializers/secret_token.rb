74078a303c10342b454d39e9627fb3f4991dec13
Richard Larcombe <rjlarcombe@gmail.com>
2013-04-11 13:49:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groshop::Application.config.secret_token = '31a22767ce39c1f3ad73f124c1f67cc115707f47e092075f75dc62435ee332faadfcb2a58a8f06aca3f5e6a173f2a83432f6345b46e0e854b6a19ff03f69e707'
